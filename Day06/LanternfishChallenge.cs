3e0be52e24183f1bfa26dff19cd14bb5c6ef0990
Tom Cowell <tom.blacknight@gmail.com>
2021-12-06 16:43:15 +0000

    feat: Completed day 6.

---
+++ Day06/LanternfishChallenge.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Day06
+{
+    public class LanternfishChallenge
+    {
+        private int[] _lanternfish;
+
+        public LanternfishChallenge(IReadOnlyList<string> inputLines)
+        {
+            _lanternfish = inputLines[0]
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        private static Dictionary<int, long> GenerateInitialLookup(int[] lanternFish)
+        {
+            var lookup = Enumerable
+                .Range(0, 9)
+                .ToDictionary(k => k, v => (long)0);
+
+            foreach (var fish in lanternFish)
+            {
+                lookup[fish]++;
+            }
+
+            return lookup;
+        }
+
+        private long RunSimulation(int days)
+        {
+            var lookup = GenerateInitialLookup(_lanternfish);
+
+            for (var day = 0; day < days; day++)
+            {
+                var zeros = lookup[0];
+
+                for (var t = 0; t < 8; t++)
+                {
+                    lookup[t] = lookup[t + 1];
+                }
+
+                lookup[8] = zeros;
+                lookup[6] += zeros;
+            }
+
+            return lookup.Sum(kv => kv.Value);
+        }
+
+        public long Part1()
+        {
+            return RunSimulation(80);
+        }
+
+        public long Part2()
+        {
+            return RunSimulation(256);
+        }
+    }
+}
---
+++ Day06/LanternfishChallengeTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Day06
+{
+    [TestFixture]
+    public class LanternfishChallengeTests : TestBase
+    {
+        public LanternfishChallengeTests() : base(6)
+        {
+        }
+
+        [Test]
+        public override void Part1_PlayerInput()
+        {
+            var challenge = new LanternfishChallenge(InputLines);
+
+            var answer = challenge.Part1();
+
+            Assert.AreEqual(386640, answer);
+        }
+
+        [Test]
+        public override void Part1_ExampleInput()
+        {
+            var challenge = new LanternfishChallenge(ExampleLines);
+
+            var answer = challenge.Part1();
+
+            Assert.AreEqual(5934, answer);
+        }
+
+        [Test]
+        public override void Part2_PlayerInput()
+        {
+            var challenge = new LanternfishChallenge(InputLines);
+
+            var answer = challenge.Part2();
+
+            Assert.AreEqual(1733403626279, answer);
+        }
+
+        [Test]
+        public override void Part2_ExampleInput()
+        {
+            var challenge = new LanternfishChallenge(ExampleLines);
+
+            var answer = challenge.Part2();
+
+            Assert.AreEqual(26984457539, answer);
+        }
+    }
+}
